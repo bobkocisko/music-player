3094369024c422f64c00bc8f5fae4f59934199cc
bobkocisko <rkocisko@gmail.com>
2014-10-08 18:29:19 -0400

    Beginning the code for selector, which will serve as the UI coordinator for showing selection options for different symbols during symbol addition or edition (WIP)

--- MusicPlayer/scripts/drawcontext.js
+++ MusicPlayer/scripts/drawcontext.js
@@ -1,10 +1,12 @@
 ﻿define(function () {
+    'use strict';
+
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 
     var _get = function () {
         return ctx;
-    }
+    };
 
     var _getColor = function (isPreview) {
         if (isPreview) {
@@ -13,10 +15,20 @@
         else {
             return "#000000";
         }
+    };
+
+    var _getEditButtonColors = function () {
+        return {
+            off: "rgba(170,0,0, 0.75)",
+            on: "rgba(170,0,0, 1)",
+            //pressed: "rgba(220,0,0, 1)",
+            //disabled: "rgba(170,170,170, 0.75)",
+            };
     }
 
     return {
         getColor: _getColor,
-        get: _get
+        getEditButtonColors: _getEditButtonColors,
+        get: _get,
     };
 })
---
+++ MusicPlayer/scripts/selector.js
@@ -0,0 +1,66 @@
+﻿define(['drawcontext', 'staffarranger','utils'], function (drawcontext, staffarranger, utils) {
+    var ctx = drawcontext.get();
+
+    var _innerRadius = staffarranger.noteHeight * 2;
+    var _outerRadius = _innerRadius + staffarranger.noteHeight * 4;
+    var _centerPointXOffset = -staffarranger.noteHeight / 2;
+    var _buttonColors = drawcontext.getEditButtonColors();
+    var optionstatuses = {
+        off: "off",
+        on: "on",
+    }
+
+    var _Selector = function () {
+        this.options = [];
+    };
+
+    _Selector.prototype = function () {
+        var _addOption = function (option, drawer) {
+            this.options.push(
+                {
+                    option: option,
+                    drawer: drawer,
+                    status: optionstatuses.off
+                });
+        };
+
+        var _draw = function (centerPoint) {
+            var arcSize = Math.PI / this.options.length;
+            for (var i = 0; i < this.options.length; i++) {
+                var option = this.options[i];
+                ctx.beginPath();
+                ctx.fillStyle = _buttonColors[option.status];
+                ctx.arc(centerPoint.x, centerPoint.y, _innerRadius, arcSize * i, arcSize * (i + 1));
+                ctx.arc(centerPoint.x, centerPoint.y, _outerRadius, arcSize * i, arcSize * (i + 1));
+                ctx.fill();
+            }
+        };
+
+        var _getDrawRect = function (centerPoint) {
+            return new utils.Rectangle()
+        };
+
+        var _updateOptionStatuses = function (point) {
+            // Updates the statuses of each option depending upon whether that option is under the specified point.
+            // Returns whether there are any status changes as a result of this update.
+        };
+
+        var _clearOptionStatuses = function () {
+            for (var i in this.options) {
+                this.options[i].status = optionstatuses.off;
+            }
+        };
+
+        return {
+            addOption: _addOption,
+            draw: _draw,
+            getDrawRect: _getDrawRect,
+            updateOptionStatuses: _updateOptionStatuses,
+            clearOptionStatuses: _clearOptionStatuses,
+        };
+    }();
+
+    return {
+        Selector: _Selector,
+    };
+});
